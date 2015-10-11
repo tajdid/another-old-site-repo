98e4192fde02ebda59e2fa81b8105cf25e205094
tajdid <tajdidrahman@gmail.com>
2015-10-11 12:45:13 +0100

    Added index

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,5 @@ $(document).ready(function(){
   // Target your .container, .wrapper, .post, etc.
   $(".post").fitVids();
 });
+
+function recentTrack(e){var t=(new Array).concat(e.recenttracks.track)[0];document.getElementById("recentTrackArtist").innerHTML=t.artist["#text"],document.getElementById("recentTrackTitle").href=t.url,document.getElementById("recentTrackTitle").innerHTML=t.name}
